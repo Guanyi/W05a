163bb7f56c3b6d827489de36025423f2b304732b
Guanyi <guanyifang@hotmail.lcom>
2015-10-06 14:27:16 -0700

    https://github.com/Guanyi/W05a

---
+++ ProductData/Partials/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProductData
+{
+    [MetadataType(typeof(ProductMetaData))]
+
+    public partial class Product { }
+
+    public class ProductMetaData
+    {
+        [ScaffoldColumn(false)]
+        public int ProductID { get; set; }
+
+        public string ProductName { get; set; }
+
+        public Nullable<int> SupplierID { get; set; }
+
+        public Nullable<int> CategoryID { get; set; }
+
+        [ScaffoldColumn(false)]
+        public string QuantityPerUnit { get; set; }
+
+        public Nullable<decimal> UnitPrice { get; set; }
+
+        [ScaffoldColumn(false)]
+        public Nullable<short> UnitsInStock { get; set; }
+
+        [ScaffoldColumn(false)]
+        public Nullable<short> UnitsOnOrder { get; set; }
+
+        [ScaffoldColumn(false)]
+        public Nullable<short> ReorderLevel { get; set; }
+
+        [ScaffoldColumn(false)]
+        public bool Discontinued { get; set; }
+    }
+}
---
+++ W05a/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(W05a.Startup))]
+namespace W05a
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
